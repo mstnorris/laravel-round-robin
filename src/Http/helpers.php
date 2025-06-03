bbcacca0ed7cd13c3e022fe8291f8cf447d761b3
Michael Norris <mstnorris@users.noreply.github.com>
2025-06-03 16:05:25 +0100

    Update helpers.php
    Explicitly mark parameters as optional

--- src/Http/helpers.php
+++ src/Http/helpers.php
@@ -3,7 +3,7 @@
 use Tonystore\LaravelRoundRobin\Services\RoundRobin;
 
 if (!function_exists('schedule')) {
-    function schedule(array $teams, int $rounds = null, bool $shuffle = true, int $seed = null, bool $doubleRound = false)
+    function schedule(array $teams, ?int $rounds = null, bool $shuffle = true, ?int $seed = null, bool $doubleRound = false)
     {
         return RoundRobin::makeSchedule($teams, $rounds, $shuffle, $seed, $doubleRound);
     }
